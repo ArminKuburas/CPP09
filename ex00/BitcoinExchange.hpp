e5f358f3e8fa032a809484e1aff8d8dfcb3a9fae
ArminKuburas <akuburass@gmail.com>
2024-10-30 18:51:22 +0200

    Added BitcoinExchange class header. Sort of a template of what I want to do. Might need more but this is a start

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,29 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:24:49 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/29 17:24:50 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/30 18:50:33 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+class BitcoinExchange
+{
+	public:
+		run(const std::string &databaseFile);
+		void processInput(const std::string &inputFile) const;
+	private:
+	std::map<std::string, double> _database;
+	
+	void loadDatabase(const std::string &databaseFile);
+	void findClosestPrice(const std::string &date) const;
+	bool isDateValid(const std::string &date) const;
+	bool isValueValid(const std::string &price) const;
+};
