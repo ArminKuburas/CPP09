d6dfd8f1fa8940fe8a54cd29f8ada90393246902
ArminKuburas <akuburass@gmail.com>
2024-10-31 09:58:04 +0200

    feat: Added the data set as well as updated the main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,28 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:24:52 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/29 17:24:53 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/31 09:57:44 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./bitcoin_exchange [input_file]" << std::endl;
+		return (1);
+	}
+	try
+	{
+		BitcoinExchange exchange(data.csv);
+		exchange.processInput(argv[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+	return (0);
+}
