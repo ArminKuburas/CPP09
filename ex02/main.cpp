351fa901d6672a4e98fe4b1c6e34b52480c82623
ArminKuburas <akuburass@gmail.com>
2024-12-09 12:39:02 +0200

    added main

---
+++ ex02/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 12:36:31 by akuburas          #+#    #+#             */
+/*   Updated: 2024/12/09 12:38:38 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./PmergeMe [string of unique numbers]" << std::endl;
+		return (1);
+	}
+}
