5e94d82598811ab58b13ab0070ba0151e8192f79
Armin Kuburas <akuburass@gmail.com>
2024-12-10 10:53:16 +0200

    Added in expanded initial validation of input

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,20 +6,29 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:36:31 by akuburas          #+#    #+#             */
-/*   Updated: 2024/12/09 12:51:25 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/12/10 10:52:50 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-bool validate_input(std::string input)
+bool validate_input(int argc, char **argv)
 {
-	std::string::iterator it = input.begin();
-	while (it != input.end())
+	bool space_collector = true;
+	for (int i = 1; i <= argc; i++)
 	{
-		if (std::isdigit(*it) == false && *it != ' ')
-			return (false);
-		it++;
+		std::string input = argv[i];
+		std::string::iterator it = input.begin();
+		while (it != input.end())
+		{
+			if (std::isdigit(*it) == false && (*it != ' ' || (space_collector == true && *it == ' ')))
+				return (false);
+			else if ((space_collector = true && std::isdigit(*it) == true))
+				space_collector = false;
+			else if (*it == ' ')
+				space_collector = true;
+			it++;
+		}
 	}
 	return (true);
 }
@@ -28,12 +37,13 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "Usage: ./PmergeMe [string of unique numbers]" << std::endl;
+		std::cerr << "Usage: ./PmergeMe [string of unique numbers];\nUsage: ./PmergeMe [multiple] [strings] [of] [input];\n./PmergeMe [mixed inputs]" << std::endl;
 		return (1);
 	}
-	if (validate_input(argv[1]) == false)
+	if (validate_input(argc, argv) == false)
 	{
 		std::cerr << "Invalid input" << std::endl;
 		return (1);
 	}
+	return (0);
 }
