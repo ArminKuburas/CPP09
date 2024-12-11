c7f6dc6c04f041b1540f98f8dd3654c8cc27ac4d
ArminKuburas <akuburass@gmail.com>
2024-12-11 19:09:11 +0200

    feat: added a lot of vector algorithm math

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 11:25:02 by akuburas          #+#    #+#             */
-/*   Updated: 2024/12/11 12:28:08 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/12/11 19:00:57 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ MergeInsert &MergeInsert::operator=(const MergeInsert &copy)
 
 void MergeInsert::VectorAlgorithm(std::string original_string)
 {
-	if (this->_original_vector.size() > 0)
-		this->_original_vector.clear();
+	if (this->_vector.size() > 0)
+		this->_vector.clear();
 	std::cout << "VectorAlgorithm called" << std::endl;
 	std::string temp_string;
 	int temp_int;
@@ -53,13 +53,65 @@ void MergeInsert::VectorAlgorithm(std::string original_string)
 		if (original_string[i] == ' ')
 		{
 			temp_int = std::stoi(temp_string);
-			_original_vector.push_back(temp_int);
+			_vector.push_back(temp_int);
 			temp_string.clear();
 		}
 		else
 			temp_string += original_string[i];
 	}
+	VectorRecursive(1);
 	
+}
+
+void MergeInsert::VectorRecursive(int pair_size)
+{
+	int	pair_amount = _vector.size() / pair_size;
+	if (pair_amount < 2)
+		return;
+	bool is_odd = pair_amount % 2 == 1;
+
+	std::vector<int>::iterator start = _vector.begin();
+	std::vector<int>::iterator end = (VecFindNextIterator(VecFindNextIterator(_vector.begin(), pair_size * pair_amount), -(is_odd * pair_size)));
+	
+	VecSwapPair(start, end, pair_size);
+	VectorRecursive(pair_size * 2);
+	std::vector<std::vector<int>::iterator> pending_insertion_chain;
+	std::vector<std::vector<int>::iterator> main_chain;
+
+	main_chain.insert(main_chain.end(), VecFindNextIterator(_vector.begin(), pair_size - 1));
+	main_chain.insert(main_chain.end(), VecFindNextIterator(_vector.begin(), pair_size * 2 - 1));
+	
+	VecChainingTime(main_chain, pending_insertion_chain, pair_size, pair_amount);
+}
+
+void VecChainingTime(std::vector<std::vector<int>::iterator> & main_chain, std::vector<std::vector<int>::iterator> & pending_insertion_chain, int pair_size, int pair_amount)
+{
+	for (int i = 4; i <= pair_amount; i += 2)
+	{
+		pending_insertion_chain.insert(pending_insertion_chain.end(), VecFindNextIterator(_vector.begin(), pair_size * (i - 1) - 1));
+		main_chain.insert(main_chain.end(), VecFindNextIterator(_vector.begin(), pair_size * i - 1));
+	}
+	int jacobsthal_prev = JacobsthalNumber(1);
+}
+
+void MergeInsert::VecSwapPair(std::vector<int>::iterator start, vector<int>::iterator end, int pair_size)
+{
+	int jump_forward = pair_size * 2;
+	for (std::vector<int>::iterator it = start; it != end; std::advance(it, jump_forward))
+	{
+		std::vector<int>::iterator this_pair = VecFindNextIterator(it, pair_size - 1);
+		std::vector<int>::iterator that_pair = VecFindNextIterator(it, pair_size * 2 - 1);
+		if (*this_pair > *that_pair)
+		{
+			std::vector<int>::iterator swap_start = VecFindNextIterator(this_pair, -pair_size + 1);
+			std::vector<int>::iterator swap_end = VecFindNextIterator(swap_start, pair_size);
+			while (swap_start != swap_end)
+			{
+				std::iter_swap(swap_start, VecFindNextIterator(swap_start, pair_size));
+				swap_start++;
+			}
+		}
+	}
 }
 
 void MergeInsert::DequeAlgorithm(std::string original_string)
@@ -80,4 +132,15 @@ void MergeInsert::DequeAlgorithm(std::string original_string)
 		else
 			temp_string += original_string[i];
 	}
-}	
+}
+
+std::vector<int>::iterator MergeInsert::VecFindNextIterator(std::vector<int>::iterator it, int steps)
+{
+	std::advance(it, steps);
+	return it;
+}
+
+int MergeInsert::JacobsthalNumber(int n)
+{
+	return (())
+}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 22:09:29 by akuburas          #+#    #+#             */
-/*   Updated: 2024/12/11 12:13:56 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/12/11 19:01:46 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <vector>
 #include <algorithm>
 #include <deque>
+#include <cmath>
 
 class MergeInsert
 {
@@ -28,8 +29,12 @@ class MergeInsert
 		void VectorAlgorithm(std::string original_string);
 		void DequeAlgorithm(std::string original_string);	
 	private:
-		std::vector<int> _original_vector;
+		std::vector<int> _vector;
 		std::deque<int> _original_deque;
-		std::vector<int> _sorted_vector;
 		std::deque<int> _sorted_deque;
+		std::vector<int>::iterator VecFindNextIterator(std::vector<int>::iterator it, int steps);
+		void VectorRecursive(int pair_size);
+		void VecSwapPair(std::vector<int>::iterator start, vector<int>::iterator end, int pair_size);
+		void VecChainingTime(std::vector<std::vector<int>::iterator> & main_chain, std::vector<std::vector<int>::iterator> & pending_insertion_chain, int pair_size, int pair_amount);
+		int JacobsthalNumber(int n);
 };
