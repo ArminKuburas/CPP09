00a48982087f6c8a4640c8f7e33678f92496cf19
Armin Kuburas <akuburass@gmail.com>
2025-02-19 15:57:48 +0200

    adding descriptors@

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:36:31 by akuburas          #+#    #+#             */
-/*   Updated: 2025/01/16 12:07:47 by akuburas         ###   ########.fr       */
+/*   Updated: 2025/02/19 14:56:12 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 #include <set>
 #include <iomanip>
 
+/**
+ * Validates a single input argument
+ * @param input The input string to validate.
+ * @return True if the input is a valid positive integer within the int range, false otherwise.
+ */
 bool validate_individual_argument(std::string input)
 {
 	if (input[0] == '-')
@@ -25,7 +30,7 @@ bool validate_individual_argument(std::string input)
 	long converted_number = strtol(input.c_str(), NULL, 10);
 	if (converted_number == 0 && input != "0")
 	{
-		std::cerr << "Invalid Input: non/number argument was provided" << std::endl;
+		std::cerr << "Invalid Input: non-number argument was provided" << std::endl;
 		return (false);
 	}
 	if (converted_number > INT_MAX || errno == ERANGE)
@@ -40,6 +45,13 @@ bool validate_individual_argument(std::string input)
 	return (true);
 }
 
+/**
+ * Validates and processes input arguments into a vector.
+ * 
+ * @param argc The argument count.
+ * @param argv The argument vector.
+ * @return True if all inputs are valid and unique, false otherwise.
+ */
 bool validate_input_vec(int argc, char **argv)
 {
 	std::vector<long> numbers;
@@ -59,6 +71,13 @@ bool validate_input_vec(int argc, char **argv)
 	return (true);
 }
 
+/**
+ * Validates and processes input arguments into a deque.
+ * 
+ * @param argc The argument count.
+ * @param argv The argument vector.
+ * @return True if all inputs are valid and unique, false otherwise.
+ */
 bool validate_input_deq(int argc, char **argv)
 {
 	std::deque<long> numbers;
@@ -78,6 +97,13 @@ bool validate_input_deq(int argc, char **argv)
 	return (true);
 }
 
+/**
+ * Converts command-line arguments into a single space-separated string.
+ * 
+ * @param argc The argument count.
+ * @param argv The argument vector.
+ * @return A string containing all arguments concatenated with spaces.
+ */
 std::string argv_to_string(int argc, char **argv)
 {
 	std::string return_string = argv[1];
@@ -97,10 +123,14 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: ./PmergeMe [unique_number] [unique_number] [unique_number]" << std::endl;
 		return (1);
 	}
+	if (validate_input_vec(argc, argv) == false)
+		return (1);
+	std::cout << "Before: " << argv_to_string(argc, argv) << std::endl;
 	clock_t vec_start = clock();
 	if (validate_input_vec(argc, argv) == false)
 		return (1);
 	std::vector<int> sorted_vec = sorter.VectorAlgorithm(argv_to_string(argc, argv));
+	std::cout << "Vector After: " << vector_to_string(sorted_vec) << std::endl;
 	clock_t vec_end = clock();
 	double elapsed_vec = static_cast<double>(vec_end - vec_start) / CLOCKS_PER_SEC;
 
@@ -108,11 +138,9 @@ int main(int argc, char **argv)
 	if (validate_input_deq(argc, argv) == false)
 		return (1);
 	std::deque<int> sorted_deq = sorter.DequeAlgorithm(argv_to_string(argc, argv));
+	std::cout << "Deque After: " << deque_to_string(sorted_deq) << std::endl;
 	clock_t deq_end = clock();
 	double elapsed_deq = static_cast<double>(deq_end - deq_start) / CLOCKS_PER_SEC;
-	std::cout << "Before: " << argv_to_string(argc, argv) << std::endl;
-	std::cout << "After: " << vector_to_string(sorted_vec) << std::endl;
-	std::cout << "After: " << deque_to_string(sorted_deq) << std::endl;
 	std::cout << "VectorAlgorithm (Which users the std::vector container) took " << std::fixed << std::setprecision(6) << elapsed_vec << " seconds" << std::endl;
 	std::cout << "DequeAlgorithm (Which uses the std::deque container) took " << std::fixed << std::setprecision(6) << elapsed_deq << " seconds" << std::endl;
 	
